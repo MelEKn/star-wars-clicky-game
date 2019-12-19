01dcf8660c771378a3c9ca2f747a63513d273b5e
Melissa Knapp <melknapp@gmail.com>
2019-12-18 20:34:14 -0500

    Static and without formatting, seems to correctly calculate what the user clicked, and whether the user wins or loses

--- src/App.js
+++ src/App.js
@@ -11,71 +11,84 @@ class App extends React.Component {
     score: 0
   };
 
- chooseCharacter = id => {
+  chooseCharacter = id => {
 
-  console.log("Characters is");
-  console.log(characters);
 
-   console.log(this.state.charactersChosen);
+    console.log("this.state.charactersChosen is");
+    console.log(this.state.charactersChosen);
 
-   this.setState({
-     charactersChosen: [...this.state.charactersChosen, id]
-   })
+    console.log("this.state.charactersChosen.includes(id) is ");
+    console.log(this.state.charactersChosen.includes(id));
 
-   console.log("this.state.charactersChosen is ");
-   console.log(this.state.charactersChosen);
-  
-  //  let chars = this.state.charactersChosen;
+    if (!this.state.charactersChosen.includes(id)) {
+      if (this.state.score >= 11) {
+        console.log("You won!");
+      }
+      else {
+        this.setState({
+          charactersChosen: [...this.state.charactersChosen, id],
+          score: this.state.score + 1
+        })
+      }
+    }
+    else {
+      console.log("You lose!")
+    }
 
-  //  console.log("typeof this.state.charactersChosen");
-  //  console.log(typeof this.state.charactersChosen);
-   
+    console.log("this.state is ");
+    console.log(this.state);
 
-  //  console.log(chars);
+    //  let chars = this.state.charactersChosen;
 
-  //  console.log("next is typeof chars");
-  //  console.log(typeof chars);
+    //  console.log("typeof this.state.charactersChosen");
+    //  console.log(typeof this.state.charactersChosen);
 
-  // if(chars.includes(id)){
-  //   this.setState({
-  //     charactersChosen: [],
-  //     score: 0
-  //   })
-  //   console.log("You lost!");
-  // } 
-  // else{
-  //  chars.push(id);
-  //   this.setState({
-  //     score: this.state.score + 1,
-  //     charactersChosen: this.state.charactersChosen.push(id),
-  //   })
-  // }
- }
+
+    //  console.log(chars);
+
+    //  console.log("next is typeof chars");
+    //  console.log(typeof chars);
+
+    // if(chars.includes(id)){
+    //   this.setState({
+    //     charactersChosen: [],
+    //     score: 0
+    //   })
+    //   console.log("You lost!");
+    // } 
+    // else{
+    //  chars.push(id);
+    //   this.setState({
+    //     score: this.state.score + 1,
+    //     charactersChosen: this.state.charactersChosen.push(id),
+    //   })
+    // }
+  }
 
   render() {
-  return (
-    <Wrapper>
-      {console.log("this.state is")}
-      {console.log(this.state)}
-      <div className="jumbotron mt-2">
-      <h1 className="title">Star Wars Clicky Game!</h1>
-      </div>
-      {this.state.characters.map(item => 
-      <CharacterCard 
-      name={item.name}
-      image={item.image}
-      id={item.id}
-      chooseCharacter ={this.chooseCharacter}
+    return (
+      <Wrapper>
+        {console.log("this.state is")}
+        {console.log(this.state)}
+        <div className="jumbotron mt-2">
+          <h1 className="title">Star Wars Clicky Game!</h1>
+        </div>
+        {this.state.characters.map(item =>
+          <CharacterCard
+            name={item.name}
+            image={item.image}
+            id={item.id}
+            chooseCharacter={this.chooseCharacter}
 
-     // removeFriend={this.removeFriend}
-      /> )}
-
-
+          // removeFriend={this.removeFriend}
+          />)}
 
 
-    </Wrapper>
-  ); 
-}
+
+
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
